f2afed6132efef10524f47e748b6bac8f8e6bb11
wangxuxiao <wxx@moolink.com>
2022-02-14 14:50:23 +0800

    修正teabar定义错误

--- app/teabar/define_teabar_app.h
+++ app/teabar/define_teabar_app.h
@@ -1,7 +1,7 @@
 #ifndef __DEFINE_TEABAR_APP_H_
 #define __DEFINE_TEABAR_APP_H_
 
-	#define AppType_TeaBar_Default (AppType_TeaBarLib<<8)
+	#define AppType_TeaBar_Default (AppTypeLib_TeaBar<<8)
 	#define AppType_TeaBar_01 AppType_TeaBar_Default+0x01
 
 
