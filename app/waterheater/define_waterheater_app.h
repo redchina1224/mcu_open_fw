81bf1bcc6c98bf1ce719bf64181f73dc90eac43e
wangxuxiao <wxx@moolink.com>
2022-02-21 23:41:13 +0800

    修正定义错误

--- app/waterheater/define_waterheater_app.h
+++ app/waterheater/define_waterheater_app.h
@@ -1,7 +1,7 @@
 #ifndef __DEFINE_WATERHEATER_APP_H_
 #define __DEFINE_WATERHEATER_APP_H_
 
-	#define AppType_WaterHeater_Default (AppType_WaterHeaterLib<<8)
+	#define AppType_WaterHeater_Default (AppTypeLib_WaterHeater<<8)
 	#define AppType_WaterHeater_01 AppType_WaterHeater_Default+0x01
 
 
