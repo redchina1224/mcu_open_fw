b6de98a75837d3a601d45e37f615ef408a148b63
wangxuxiao <wxx@moolink.com>
2022-02-28 11:08:27 +0800

    app代码模块方案变更测试

--- app/waterheater/waterheater_app.h
+++ app/waterheater/waterheater_app.h
@@ -13,11 +13,48 @@
 //******************************************************************
 
 
-/*
-#include "teabar_boil_water_app.h"
-#include "teabar_warm_water_app.h"
-#include "teabar_take_water_app.h"
-*/
+//----机器状态定义
+#define			MACHINE_POWER_ON			0			//上电
+#define			MACHINE_IDEL				1			//待机
+#define			MACHINE_WORK_TEMP				2			//正常工作模式
+#define			MACHINE_WORK_POWER		3
+#define			MACHINE_SELF_CHECK	4			//自检
+#define			MACHINE_ERROR				200			//故障
+
+
+#include "position_pid_app.h"
+
+
+struct zd_app_set_t {
+unsigned char temp;//温度设定
+unsigned char power;//功率设定
+unsigned char mode;//上电模式设定
+}
+
+
+struct zd_app_work_t {
+unsigned char intemp;//温度设定
+unsigned char outtemp;//温度设定
+unsigned int waterin_ad;
+unsigned int waterout_ad;
+unsigned char power;//功率设定
+unsigned char mode;//上电模式设定
+unsigned char water_speed;
+}
+
+
+
+
+
+struct  zd_app_t {
+struct zd_app_set_t set;
+struct zd_app_work_t work;
+struct zd_pospid_t pid;
+};
+
+
+
+
 
 void app_init(void);
 void App_Run(void);
