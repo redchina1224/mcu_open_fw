88b039b89809e6ec02b45fbe3f06fee5eec1936d
wangxuxiao <wxx@moolink.com>
2022-03-22 13:07:04 +0800

    softdig8-add

--- drv/display/loadDisplay_drv.h
+++ drv/display/loadDisplay_drv.h
@@ -6,6 +6,8 @@
 		#include "softled\softled_drv.h"
 	#elif ((DisplayType&DisplayType_SoftLed_Bits)==DisplayType_SoftLed_Bits) 
 		#include "softled_bits\softled_bits_drv.h"
+	#elif ((DisplayType&DisplayType_SoftLed_Dig8)==DisplayType_SoftLed_Dig8) 
+		#include "softled_dig8\softled_dig8_drv.h"
 	#elif (DisplayType==DisplayType_Dig8IcLed_tm1628)
 		#include "ic_led\tm1628\tm1628_drv.h"
 	#elif (DisplayType==DisplayType_Unkonw)
