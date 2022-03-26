45ed12e1c3f1a3f3210c532e499a4a314ed79bd4
wangxuxiao <wxx@moolink.com>
2022-03-26 15:44:44 +0800

    添加设置闪烁与超时自动退出功能

---
+++ app/general/settingdisplayblink/setting_display_blink_app.c
@@ -0,0 +1,56 @@
+//******************************************************************************
+//Copyright(C) 2020-2021 zhejiang zhida dianzikeji Co., Ltd
+//浙江智达电子科技有限公司
+//File name:      setting_display_blink_app.c
+//Author:         zzg
+//Version:        Ver0.1
+//Date:           2021-4-25
+//Description:    设置闪烁显示与超时退出功能。
+//History:
+//******************************************************************************
+
+
+
+unsigned char settingDisplayBlink(struct  zd_setting_display_blink_t* setting )
+{
+
+		if(setting->exitTimeOut_x100ms>0) setting->exitTimeOut_x100ms--;//超时自动退出
+		else
+		{
+			return 0;
+		}
+
+
+		if(setting->blinkTimeOut_x100ms>0) setting->blinkTimeOut_x100ms--;//当前时间大于前次设置的闪烁结束时间后触发一次切换
+		else
+		{
+			if(setting->blinkDisplayEn==0)//闪烁时是否显示或熄屏的标志
+			{
+				setting->blinkDisplayEn=1;//闪烁时是否显示或熄屏的标志,置1后，下次切换为熄屏
+			}
+			else
+			{			
+				setting->blinkDisplayEn=0;//闪烁时是否显示或熄屏的标志,置0后，下次切换为显示
+			}
+		}
+
+
+
+		return 1;
+
+
+
+
+
+}
+
+void settingDisplayBlink_Start(struct  zd_setting_display_blink_t* setting,unsigned char exitTimeSet_x100ms)
+{
+	setting->blinkTimeOut_x100ms=0;//单次闪烁计时,置0后，立即执行一次切换操作
+	setting->blinkDisplayEn=0;//闪烁时是否显示或熄屏的标志,置0后，下次切换为显示
+	setting->exitTimeOut_x100ms=exitTimeSet_x100ms-2; //闪烁5秒，时间采用4.8秒，使最后一次为熄灭状态，避免最后一次先点亮后再切换;
+}
+
+
+
+
---
+++ app/general/settingdisplayblink/setting_display_blink_app.h
@@ -0,0 +1,96 @@
+//******************************************************************************
+//Copyright(C) 2020-2021 zhejiang zhida dianzikeji Co., Ltd
+//浙江智达电子科技有限公司
+//File name:      function.h
+//Author:         zzg
+//Version:        Ver0.1
+//Date:           2021-4-25
+//Description:    功能
+//History:
+//******************************************************************************
+#ifndef __APPSTDLIB_SETTING_DISPALY_BLINK_APP_H__
+#define __APPSTDLIB_SETTING_DISPALY_BLINK_APP_H__
+//******************************************************************
+
+struct  zd_setting_display_blink_t {
+	struct {
+		unsigned char blinkDisplayEn:1;
+		unsigned char inSetting1:1;
+		unsigned char inSetting2:1;
+		unsigned char inSetting3:1;	
+		unsigned char inSetting4:1;
+		unsigned char inSetting5:1;
+		unsigned char inSetting6:1;
+		unsigned char inSetting7:1;		
+	};
+unsigned char exitTimeOut_x100ms;
+unsigned char blinkTimeOut_x100ms;
+};
+
+
+
+unsigned char settingDisplayBlink(struct  zd_setting_display_blink_t* setting );
+void settingDisplayBlink_Start(struct  zd_setting_display_blink_t* setting,unsigned char exitTimeSet_x100ms);
+
+#endif 
+
+
+
+//使用示例
+/*
+
+			//触发设置和在设置过程中
+			if(按键触发)
+			{
+				setting.inSetting1=1;
+				settingDisplayBlink_Start(&setting,50);//进入设置态,超时退出时间设置为50x100ms
+			}
+
+
+			//显示内容与超时自动保存
+			if(setting.inSetting1!=0)//在设置状态
+			{
+				if(100ms执行一次标志)
+				{
+
+					if(settingDisplayBlink(&setting))//为1时未超时显示内容
+					{
+						if(setting.blinkDisplayEn==1)//闪烁时是否显示或熄屏的标志
+						{
+							//显示设置内容
+						}
+						else
+						{			
+							//显示熄屏
+						}
+					}
+					else//为0时，设置时间超时
+					{
+						//保存设置数据
+					
+						//清除设置标志位
+						setting.inSetting1=0;
+					}
+				}
+
+
+			}
+			else//不在设置状态
+			{
+				//显示非设置态的内容
+			}
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+/************************ (C) COPYRIGHT zhejiang zhida *****END OF FILE****/
