f5fce10cc40ac7a50e96b4909f779a6b72c6f37f
wangxuxiao <wxx@moolink.com>
2024-01-24 10:37:55 +0800

    删除不常用MCU触摸调用

--- core/load_touch_core.h
+++ core/load_touch_core.h
@@ -4,14 +4,14 @@
 //按芯片载入对应头文件 并定义标准化接口
 #if (((McuType&McuType_FacMask)==McuType_CmsSemicon))
 	#include "cms\cms_touch_load_core.h"
-#elif (McuType==McuType_SinOneChip_SC92F8463B)
+//#elif (McuType==McuType_SinOneChip_SC92F8463B)
 
-	#include "soc\touch_92f8x6xb\SensorMethod.h"
-	#include "soc\touch_92f8x6xb\S_TouchKeyCFG.h"
-	
-#elif (McuType==McuType_FremontMicroDevices_FT62F13)
+//	#include "soc\touch_92f8x6xb\SensorMethod.h"
+//	#include "soc\touch_92f8x6xb\S_TouchKeyCFG.h"
+//	
+//#elif (McuType==McuType_FremontMicroDevices_FT62F13)
 
-	#include "fmd\FT62F13X\touch.h"
+//	#include "fmd\FT62F13X\touch.h"
 
 #elif (McuType==McuType_CaChip_CA51F3)
 	#include "cachip\Device\CA51F3\TS_Lib\Includes\ts_def.h"	
